154c6b73a581ad3581c77dd63184edc15fa6ec35
oscartorres098 <36300388+oscartorres098@users.noreply.github.com>
2020-12-22 11:03:43 -0500

    pureba2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-//pureba1
+//pureba222222
 // Añadir prefijos a rutas / Cargar rutas
 app.use("/user", user_routes);
 app.get("/", (req, res) => {
